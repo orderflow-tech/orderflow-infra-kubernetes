19895936a83577b980d0aebdcc388ce2e169168c
Tharlys Souza <tharlys.souza@outlook.com>
2025-10-05 11:59:50 -0300

    fix: enhance comments for clarity in KMS module configuration

--- kms.tf
+++ kms.tf
@@ -1,6 +1,7 @@
-# checkov:skip=CKV_TF_1: Using specific version commit hash for security and reproducibility
+# checkov:skip=CKV_TF_1: Using specific version for security and reproducibility
 module "cloudwatch_kms_key" {
-  source = "github.com/terraform-aws-modules/terraform-aws-kms?ref=5b76b18f5999495ae1c1ac5524de519025fcde10"
+  source  = "terraform-aws-modules/kms/aws"
+  version = "1.5.0"
 
   description             = "KMS key for EKS CloudWatch Logs encryption"
   deletion_window_in_days = 7
